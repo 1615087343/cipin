a83d77d6234314d5fd80db0030af80494f6acd7b
1615087343 <48074759+1615087343@users.noreply.github.com>
2019-03-20 00:28:34 +0800

    Add files via upload

---
+++ Demo/src/WordCount.java
@@ -0,0 +1,59 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.util.Map;
+import java.util.Scanner;
+import java.util.Set;
+import java.util.TreeMap;
+
+
+
+public class WordCount {
+	public static void main(String[] args) throws Exception{
+		BufferedReader bf=new BufferedReader(new FileReader("wj.txt"));
+		String s;
+		String[] a;//存放统计的单词的频率
+		Map<String,Integer> map=new TreeMap<String,Integer>();//集合用键值对的方式存取数据
+		while((s=bf.readLine())!=null){
+			String[] words=s.split("[^a-zA-Z]");//截取字母，避免多余的计入，比如各种符号
+			for(int i=0;i<words.length;i++){
+				String key=words[i].toLowerCase();//便于管理，将字母变小写
+				if(key.length()>0){
+					if(!map.containsKey(key)){
+						map.put(key,1);
+					}else{
+						int value=map.get(key);
+						value++;
+						map.put(key,value);
+					}
+				}
+			}			
+		}
+		//使用switch语句实现功能选择
+		System.out.println("-------词频--------");
+		System.out.println("1.单个单词的查找");
+		System.out.println("2.高频词的显示");
+		System.out.println("3.输出文件完成");
+		Scanner input = new Scanner(System.in);
+		int i=input.nextInt();	
+		switch(i){
+		case 1:
+			//提示用户输入单词，并提示用什么符号隔开
+			System.out.printf("请输入你要查找的单词，并用';'隔开");
+			String str= input.next();
+			Tongji tj=new Tongji();
+			a=tj.oneword(map,str); //通过方法调用来实现单个单词的频率
+			break;					//用break中断循环，否则一直执行，直到最后
+		case 2:		
+			Scanner sc=new Scanner(System.in);
+		    System.out.println("请输入要查看的高频词频的个数:"); 
+			int k = sc.nextInt();  
+			Tongji tj1=new Tongji();
+			tj1.gaopin(map, k);	//通过方法调用来实现单个单词的频率
+			break;
+		case 3:
+			Tongji tj2=new Tongji();
+			tj2.out(map);	//通过方法调用来存入result文件
+			break;
+		}	
+	}
+}
